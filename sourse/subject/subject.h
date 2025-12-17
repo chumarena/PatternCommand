9ef393164d8510d6226b553745fcedfff85f7e7f
Рената <r.ajusheva@gmail.com>
2025-12-17 16:56:21 +0300

    Upload subject

---
+++ sourse/subject/subject.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class subject {
+public:
+	int function(int a, int b) {
+		return a + b;
+	}
+
+	int sum_5(int a, int b, int c, int d, int e) {
+		return a + b + c + d + e;
+	}
+
+	int zero() {
+		return 12345;
+	}
+};
